82ed877def08bf504f4fb426d3c17f39e6e02f7e
lenka <loksik.lenka@gmail.com>
2022-11-07 08:37:49 +0100

    created Movie and MovieRepository

--- app/Model/Movie/Movie.php
+++ app/Model/Movie/Movie.php
@@ -1,8 +1,119 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Model\Movie;
 
 class Movie
 {
+    /**
+     * @var int
+     */
+    private $id;
 
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $countryOfOrigin;
+
+    /**
+     * @var int
+     */
+    private $releaseYear;
+
+    /**
+     * @var int|null
+     */
+    private $rating;
+
+    /**
+     * @var array
+     */
+    private $genreIds;
+
+    /**
+     * @var array
+     */
+    private $genreNames;
+
+    public function __construct(string $name, string $countryOfOrigin, int $releaseYear)
+    {
+        $this->name = $name;
+        $this->countryOfOrigin = $countryOfOrigin;
+        $this->releaseYear = $releaseYear;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getCountryOfOrigin(): string
+    {
+        return $this->countryOfOrigin;
+    }
+
+    public function setCountryOfOrigin(string $countryOfOrigin): void
+    {
+        $this->countryOfOrigin = $countryOfOrigin;
+    }
+
+    public function getReleaseYear(): int
+    {
+        return $this->releaseYear;
+    }
+
+    public function setReleaseYear(int $releaseYear): void
+    {
+        $this->releaseYear = $releaseYear;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): void
+    {
+        $this->rating = $rating;
+    }
+
+    public function getGenreIds(): array
+    {
+        return $this->genreIds;
+    }
+
+    public function setGenreIds(array $genreIds): void
+    {
+        $this->genreIds = $genreIds;
+    }
+
+    public function getGenreNames(): array
+    {
+        return $this->genreNames;
+    }
+
+    public function setGenreNames(array $genreNames): void
+    {
+        $this->genreNames = $genreNames;
+    }
 }
--- app/Model/Movie/MovieRepository.php
+++ app/Model/Movie/MovieRepository.php
@@ -1,8 +1,76 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Model\Movie;
 
-class MovieRepository
-{
+use App\Model\BaseRepository;
 
+class MovieRepository extends BaseRepository
+{
+    public function getTableName(): string
+    {
+        return 'movie';
+    }
+
+    public function getById(int $id): ?Movie
+    {
+        $row = $this->getDbConnection()->query('SELECT * FROM movie WHERE id = ?', $id)->fetch();
+
+        if ($row === null) {
+            return null;
+        }
+
+        $movie = new Movie(
+            (string)$row->name,
+            (string)$row->country_of_origin,
+            (int)$row->release_year
+        );
+        $movie->setId((int)$row->id);
+        $movie->setRating($row->rating);
+
+        return $movie;
+    }
+
+    public function mapRowsToObjects(array $rows): array
+    {
+        $movies = [];
+
+        foreach ($rows as $row) {
+            $movie = new Movie(
+                (string)$row->name,
+                (string)$row->country_of_origin,
+                (int)$row->release_year
+            );
+            $movie->setId((int)$row->id);
+            $movie->setRating($row->rating);
+
+            $movies[] = $movie;
+        }
+
+        return $movies;
+    }
+
+    /**
+     * @return Movie[]
+     * @throws \Dibi\Exception
+     */
+    public function findAll(): array
+    {
+        $rows = $this->getDbConnection()->query('SELECT * FROM movie')->fetchAll();
+
+        return $this->mapRowsToObjects($rows);
+    }
+
+    public function findAllForSelectBox(): array
+    {
+        $movies = $this->findAll();
+
+        $returnArray = [];
+
+        foreach ($movies as $movie) {
+            $returnArray[$movie->getId()] = $movie->getName();
+        }
+
+        return $returnArray;
+    }
 }
